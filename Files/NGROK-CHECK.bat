fabfb9cd5006d085dc715a341e3eecb91bc6b83b
m0rningstar999 <56830409+m0rningstar999@users.noreply.github.com>
2021-09-01 10:04:46 +0630

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -7,9 +7,9 @@ net localgroup administrators administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Administrator
-echo Pass: @#Coffin123456
-curl -O https://raw.githubusercontent.com/meru-golang/coffinRDP/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+echo User: admin
+echo Pass: morning9@
+curl -O https://raw.githubusercontent.com/m0rningstar999/windowsRDP-c9ffrn/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\VScode-installer.exe" https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\openvpn.msi" https://www-green.openvpn.net/downloads/openvpn-connect-v3-windows.msi >out.txt 2>&1
 
